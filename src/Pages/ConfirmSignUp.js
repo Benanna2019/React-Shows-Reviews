7cda9eaf76344e86070fff81147ec6f10877c457
jdobsond <jdobsond@gmail.com>
2020-10-12 09:14:23 -0400

    added cognito for signup and confirmation code

--- src/Pages/ConfirmSignUp.js
+++ src/Pages/ConfirmSignUp.js
@@ -12,6 +12,8 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import { Auth } from 'aws-amplify';
+
 
 function Copyright() {
   return (
@@ -46,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ConfirmSignUp() {
+export default function ConfirmSignUp({username}) {
   const classes = useStyles();
 
   return (
@@ -59,7 +61,21 @@ export default function ConfirmSignUp() {
         <Typography component="h1" variant="h5">
           Confirm Sign Up
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} noValidate onSubmit={(e) => {
+              e.preventDefault();
+              const code = e.target.elements.code.value;
+              (async function () {
+                try {
+                  const resp = await Auth.confirmSignUp(username, code);
+                  console.log("SUCCESSFULLY AUTHENTICATED")
+                  console.log(resp)
+                } catch (error) {
+                    console.log('error confirming sign up', error);
+                }
+            })();
+      
+
+            }}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
