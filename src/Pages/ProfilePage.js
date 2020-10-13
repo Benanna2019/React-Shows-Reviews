af417244f8c0a8ce602c6b02ea137e8fc4fbdea8
rwfarish <69471840+rwfarish@users.noreply.github.com>
2020-10-13 09:28:13 -0400

    update profile page

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,5 +1,60 @@
 import React from "react";
+import axios from "axios";
 
-export default function ProfilePage() {
-  return <div>This is the profile page.</div>;
+export default function ProfilePage({ signedIn }) {
+  const [currentUser, setCurrentUser] = React.useState(undefined);
+  React.useEffect(() => {
+    (async function () {
+      try {
+        const token = signedIn.signInUserSession.idToken.jwtToken;
+        const response = await axios.post("http://localhost:4000/get-user", {
+          token,
+        });
+        setCurrentUser(response);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+  console.log(currentUser);
+  return (
+    <div>
+      <div>Profile Page</div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const token = signedIn.signInUserSession.idToken.jwtToken;
+          const aboutMe = e.target.elements.aboutMe.value;
+          const profilePic = "";
+          const age = e.target.elements.age.value;
+          (async function () {
+            try {
+              await axios.put("http://localhost:4000/update-user", {
+                token,
+                aboutMe,
+                profilePic,
+                age,
+              });
+            } catch (error) {
+              console.log(error);
+            }
+          })();
+        }}
+      >
+        <h3>{currentUser && currentUser.data.username}</h3>
+        <input type="file"></input>
+        <input
+          type="text"
+          name="aboutMe"
+          placeholder={currentUser && currentUser.data.aboutMe}
+        ></input>
+        <input
+          type="text"
+          name="age"
+          placeholder={currentUser && currentUser.data.age}
+        ></input>
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
 }
--- src/components/PrivateRoutes.js
+++ src/components/PrivateRoutes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Router } from "@reach/router";
 import Home from "../Pages/Home";
-import ProfilePage from "../Pages/ProfilePage"; 
+import ProfilePage from "../Pages/ProfilePage";
 
-export default function PrivateRoutes({ setSignedIn }) {
+export default function PrivateRoutes({ setSignedIn, signedIn }) {
   return (
     <Router>
       <Home path="/home" setSignedIn={setSignedIn} />
-      <ProfilePage path="/profile" />
+      <ProfilePage path="/profile" signedIn={signedIn} />
     </Router>
   );
 }
